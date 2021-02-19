e52e3a5c26915fec0a662056468e6496074027db
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2021-02-19 14:26:05 +0100

    added filters config

--- src/pages/Filters/Filters.tsx
+++ src/pages/Filters/Filters.tsx
@@ -1,8 +1,211 @@
-import React from "react"
+import React, { useState } from "react"
 import { RouterProps } from "../App"
+import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap"
+import "./Filters.css"
+import keccak256 from "keccak256";
+
+enum Visibility {
+  None,
+  Private,
+  Shared,
+  ThirdParty
+}
+
+interface FilterList {
+  name: string
+  cidHashes: string[]
+  visibility: Visibility
+}
 
 function Filters({ match }: RouterProps) {
-  return <h2>Filters</h2>;
+  const [filerLists, setFilerLists] = useState<FilterList[]>([])
+
+  const [showAdd, setShowAdd] = useState(false);
+  const handleCloseAdd = () => setShowAdd(false);
+  const handleShowAdd = () => setShowAdd(true);
+
+  const [showImport, setShowImport] = useState(false);
+  const handleCloseImport = () => setShowImport(false);
+  const handleShowImport = () => setShowImport(true);
+
+  const [name, setName] = useState<string>("");
+  const [cidHashes, setCidHashes] = useState<string[]>([]);
+
+  const addFilter = () => {
+    handleCloseAdd()
+
+    const filterList: FilterList = {
+      name,
+      cidHashes,
+      visibility: Visibility.Private
+    }
+
+    // be kind rewind
+    setName("")
+    setCidHashes([])
+
+    setFilerLists([
+      ...filerLists,
+      filterList
+    ])
+  }
+
+  const importFilter = () => {
+    handleCloseImport()
+
+    // todo: implement
+
+    console.log("filter imported")
+  }
+
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    setName(event.target.value)
+  }
+
+  const cidsChanged = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    event.preventDefault()
+    const cids = event.target.value.split("\n")
+    const cidHahes = cids.map((cid) =>
+      keccak256(Buffer.from(cid)).toString('hex')
+    )
+
+    setCidHashes(cidHahes)
+    console.log(cidHahes)
+  }
+
+  const translateVisibility = (visibility: Visibility): string => {
+    let result = ""
+
+    switch (visibility) {
+      case Visibility.Private:
+        result = "Private"
+        break;
+      case Visibility.Shared:
+        result = "Shared"
+        break;
+      case Visibility.ThirdParty:
+        result = "3rd Party"
+        break;
+
+    }
+
+    return result
+  }
+
+  return (
+    <>
+      <Container>
+        <h2>Filters</h2>
+        <Row>
+          <Col>
+            <Form inline>
+              <Form.Group controlId="search">
+                <Form.Control type="text" placeholder="Search CID"/>
+                <Form.Control as="select">
+                  <option>CID</option>
+                </Form.Control>
+                <Button>Submit</Button>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col className="text-right">
+            <Button className="btn-light" onClick={handleShowAdd}>+ new Filter</Button>
+            <Button className="btn-light" onClick={handleShowImport}>Import Filter</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {
+              filerLists.map((fl) => (
+                <div className={"mt-1"}>
+                  <a href={"#"}>{fl.name}</a>
+                  <span className={"ml-1 text-dim"}>
+                    [{translateVisibility(fl.visibility)}:
+                    {fl.cidHashes.length} items]
+                  </span>
+                </div>
+              ))
+            }
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal show={showAdd} onHide={handleCloseAdd} centered={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Custom Filter</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Row>
+              <Col>
+                <Form.Control onChange={changeName} type="text"
+                              placeholder="List Name"/>
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col xs={"auto"}>
+                <Form.Group controlId="visibility">
+                  <Form.Control as="select">
+                    <option>Public</option>
+                    <option>Private</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Label className={"text-dim"}>
+                  Filtered CIDs on shared lists will be accessible to other nodes.
+                </Form.Label>
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col>
+                <Form.Control onChange={cidsChanged}
+                              as="textarea" rows={5}/>
+                <Form.Label className={"text-dim"}>
+                  One CID per line. (Optional: use CID, URL to link to public record of complaint)
+                </Form.Label>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAdd}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addFilter}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showImport} onHide={handleCloseImport} centered={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Import Filter</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Row>
+              <Col>
+                <Form.Control type="text" placeholder="Share Link URL"/>
+                <Form.Label className={"text-dim"}>
+                  Input a URL to run a filter made by a 3rd party.
+                </Form.Label>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseImport}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={importFilter}>
+            Import
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Filters
--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,66 +1,74 @@
 import React from "react"
-import { FormCheck, ToggleButton, ToggleButtonGroup } from "react-bootstrap"
+import { Col, Container, FormCheck, Row, ToggleButton, ToggleButtonGroup } from "react-bootstrap"
 import "./Settings.css"
 
 export default class Settings extends React.Component<any, any> {
 
   render() {
     return (
-      <>
+      <Container>
         <h2>Settings</h2>
 
-        <div className={"settings-block"}>
-          <FormCheck
-            type="switch"
-            id="bitscreen-switch"
-            label="Filter content using BitScreen"
-          />
-          <p className="text-dim">
-            Filtering enables a node operator to decline storage and retrieval deals for known CIDs.
-          </p>
+        <Row className={"settings-block"}>
+          <Col>
+            <FormCheck
+              type="switch"
+              id="bitscreen-switch"
+              label="Filter content using BitScreen"
+            />
+            <p className="text-dim">
+              Filtering enables a node operator to decline storage and retrieval deals for known CIDs.
+            </p>
 
-          <ToggleButtonGroup
-            vertical
-            name={"select-filter"}
-            type="radio"
-            defaultValue={"filterAll"}
-          >
-            <ToggleButton value={'filterAll'}>Filter CIDs blocked by any node</ToggleButton>
-            <ToggleButton value={'filterOnlyMy'}>Only filter CIDs on my lists</ToggleButton>
-          </ToggleButtonGroup>
-        </div>
+            <ToggleButtonGroup
+              vertical
+              name={"select-filter"}
+              type="radio"
+              defaultValue={"filterAll"}
+            >
+              <ToggleButton value={'filterAll'}>Filter CIDs blocked by any node</ToggleButton>
+              <ToggleButton value={'filterOnlyMy'}>Only filter CIDs on my lists</ToggleButton>
+            </ToggleButtonGroup>
+          </Col>
 
-        <div className={"settings-block"}>
-          <FormCheck
-            type="switch"
-            id="share-lists"
-            label="Share contents of my filter lists with other nodes"
-          />
-          <p className="text-dim">
-            (Private lists will not be affected)
-          </p>
-        </div>
+        </Row>
 
-        <div className={"settings-block"}>
-          <FormCheck
-            type="switch"
-            id="enhanced-filtering"
-            label="Use enhanced filtering"
-          />
-          <p className="text-dim">
-            BitScreen can auto-filter hashes found in third party databases
-          </p>
+        <Row className={"settings-block"}>
+          <Col>
+            <FormCheck
+              type="switch"
+              id="share-lists"
+              label="Share contents of my filter lists with other nodes"
+            />
+            <p className="text-dim">
+              (Private lists will not be affected)
+            </p>
+          </Col>
+        </Row>
 
-          <FormCheck type="checkbox"
-                     label="Audible Magic (Copyrighted Music)"/>
+        <Row className={"settings-block"}>
+          <Col>
+            <FormCheck
+              type="switch"
+              id="enhanced-filtering"
+              label="Use enhanced filtering"
+            />
+            <p className="text-dim">
+              BitScreen can auto-filter hashes found in third party databases
+            </p>
 
-          <FormCheck type="checkbox"
-                     label="PhotoDNA (CSAM)"/>
+            <FormCheck type="checkbox"
+                       label="Audible Magic (Copyrighted Music)"/>
 
-          <FormCheck type="checkbox"
-                     label="GIFCT (Terrorist Content)"/>
-        </div>
-      </>
+            <FormCheck type="checkbox"
+                       label="PhotoDNA (CSAM)"/>
+
+            <FormCheck type="checkbox"
+                       label="GIFCT (Terrorist Content)"/>
+          </Col>
+        </Row>
+
+      </Container>
     );
   }
 }
