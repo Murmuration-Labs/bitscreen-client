4e84cc17e0a16cc7b33ed4d85f76b190d1be5a55
Stefan Manciu <stefan@keyko.io>
2021-05-19 11:44:57 +0300

    Made use of node env for correct server uri

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,10 @@
 export const serverUri = (): string => {
-  // return "http://localhost:3030";
-  return "https://bxn.keyko.rocks";
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return "http://localhost:3030";
+    case "production":
+      return "https://bxn.keyko.rocks";
+    default:
+      return "https://bxn.keyko.rocks";
+  }
 };
