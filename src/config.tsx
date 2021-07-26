b1796b58c1fe7ad2b5e8697025f90b1f46a87b90
Stefan Manciu <manciustefan@gmail.com>
2021-07-26 10:57:28 +0300

    Added possibility to change host from docker

--- src/config.tsx
+++ src/config.tsx
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV;
 export const serverUri = (): string => {
   switch (environment) {
     case "development":
-      return "http://localhost:3030";
+      return process.env.REACT_APP_HOST || "http://localhost:3030";
     case "production":
       return "https://bxn.keyko.rocks";
     default:
@@ -17,7 +17,7 @@ export const serverUri = (): string => {
 export const remoteMarketplaceUri = (): string => {
   switch (environment) {
     case "development":
-      return "http://localhost:3030";
+      return process.env.REACT_APP_HOST || "http://localhost:3030";
     case "production":
       // here you can set another server for prod
       return "https://bxn.keyko.rocks";
