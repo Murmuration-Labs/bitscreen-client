810d93ae86b958e2b16b007788656b4327e6064d
Alexandru Marian Constantin <alex@keyko.io>
2021-07-30 16:16:19 +0300

    BTSC-70 fetch details for filter

--- src/pages/Public/FilterDetailsPage.tsx
+++ src/pages/Public/FilterDetailsPage.tsx
@@ -1,5 +1,30 @@
+import { filter } from "lodash";
+import React, { useEffect, useState } from "react";
+import ApiService from "../../services/ApiService";
+import { CidItem, FilterList } from "../Filters/Interfaces";
+
 const FilterDetailsPage = (props) => {
-  const filterId = props.match.params.id;
-  return <div>Hello {filterId}</div>;
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [filterDetails, setFilterDetails] = useState({});
+
+  const loadFilter = (id: number): void => {
+    ApiService.getPublicFilterDetails(id).then((data: any) => {
+      const details = {
+        filterName: data.filter.name,
+        subscribersCount: data.filter.provider_Filters.length - 1,
+        cidsCount: data.filter.cids.length,
+        provider: data.provider,
+        lastUpdated: data.filter.updated,
+      };
+      setFilterDetails(details);
+      setLoaded(true);
+    });
+  };
+
+  useEffect(() => {
+    loadFilter(props.match.params.id as number);
+  }, [props.match.params.id]);
+
+  return <>{loaded ? <div>Hello world</div> : null}</>;
 };
 export default FilterDetailsPage;
