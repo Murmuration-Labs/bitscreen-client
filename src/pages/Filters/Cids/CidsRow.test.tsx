d106819bb79a018ae323896024d9dfe7470d7049
Alexandru-Marian Constantin <alex@keyko.io>
2021-10-05 14:43:02 +0300

    Commited test files so I can switch branch to finalize design

--- src/pages/Filters/Cids/CidsRow.test.tsx
+++ src/pages/Filters/Cids/CidsRow.test.tsx
@@ -1,193 +1,189 @@
-import App from "../../App";
-import React from "react";
-import { configure, mount, shallow } from "enzyme";
-import CidsRow from "./CidsRow";
-import {CidItem, Visibility} from "../Interfaces";
+import { Table, TableBody, TableContainer, TableRow } from "@material-ui/core";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import {SnackbarProvider} from "notistack";
-import {Table, TableBody, TableContainer, TableRow, IconButton} from "@material-ui/core";
-import { useSnackbar, VariantType, WithSnackbarProps } from 'notistack';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { configure, mount, shallow } from "enzyme";
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import CidsRow from "./CidsRow";
+import FilterPage from "../FilterPage";
 
 configure({ adapter: new Adapter() });
 
 const mockEnqueue = jest.fn();
-jest.mock('notistack', () => ({
-    ...jest.requireActual('notistack'),
-    useSnackbar: () => {
-        return {
-            enqueueSnackbar: mockEnqueue
-        };
-    }
+jest.mock("notistack", () => ({
+  ...jest.requireActual("notistack"),
+  useSnackbar: () => {
+    return {
+      enqueueSnackbar: mockEnqueue,
+    };
+  },
 }));
 
 const props = {
-    filter: {
-        "providerId": 1,
-        "created": "2021-09-21T13:56:33.039Z",
-        "updated": null,
-        "id": 5,
-        "name": "Edited from cli v2",
-        "description": "Test 1234 test",
-        "override": false,
-        "enabled": true,
-        "visibility": 2,
-        "shareId": "fb21-6957-03d2-2d40",
-        "cids": [
-            {
-                "created": "2021-09-21T13:56:33.058Z",
-                "updated": null,
-                "id": 17,
-                "cid": "sdafds",
-                "refUrl": "",
-                "tableKey": "1",
-                "isChecked": false,
-                "isSaved": true
-            }
-        ],
-        "provider": {
-            "created": "2021-09-10T09:35:54.713Z",
-            "updated": "2021-09-28T12:43:20.507Z",
-            "id": 1,
-            "walletAddressHashed": "d4539a7862192266906ff074c49ac3e5989d2067c82627261fd32d5e4bd4e091",
-            "country": "Romania",
-            "businessName": "Stefan123",
-            "website": "fsdfsd.com",
-            "email": "manciustefan@gmail.com",
-            "contactPerson": "asfsada",
-            "address": "dsadsads",
-            "nonce": "e0fbadb3-ef17-4a49-b47d-47568adaf978"
-        }
+  filter: {
+    providerId: 1,
+    created: "2021-09-21T13:56:33.039Z",
+    id: 5,
+    name: "Edited from cli v2",
+    description: "Test 1234 test",
+    override: false,
+    enabled: true,
+    visibility: 2,
+    shareId: "fb21-6957-03d2-2d40",
+    cids: [
+      {
+        created: "2021-09-21T13:56:33.058Z",
+        id: 17,
+        cid: "sdafds",
+        refUrl: "",
+        tableKey: "1",
+        isChecked: false,
+        isSaved: true,
+      },
+    ],
+    provider: {
+      created: "2021-09-10T09:35:54.713Z",
+      updated: "2021-09-28T12:43:20.507Z",
+      id: 1,
+      walletAddressHashed:
+        "d4539a7862192266906ff074c49ac3e5989d2067c82627261fd32d5e4bd4e091",
+      country: "Romania",
+      businessName: "Stefan123",
+      website: "fsdfsd.com",
+      email: "manciustefan@gmail.com",
+      contactPerson: "asfsada",
+      address: "dsadsads",
+      nonce: "e0fbadb3-ef17-4a49-b47d-47568adaf978",
     },
-    cid: {
-        "created": "2021-09-21T13:56:33.058Z",
-        "updated": null,
-        "id": 17,
-        "cid": "sdafds",
-        "refUrl": "",
-        "tableKey": "1",
-        "isChecked": false,
-        "isSaved": true
-    }
-}
+  },
+  cid: {
+    created: "2021-09-21T13:56:33.058Z",
+    id: 17,
+    cid: "sdafds",
+    refUrl: "",
+    tableKey: "1",
+    isChecked: false,
+    isSaved: true,
+  },
+};
 
 describe("Cid row tests", () => {
-    it("Should render a CID row", () => {
-        const page = mount(
-            <SnackbarProvider>
-                <TableContainer>
-                    <Table size={"small"} stickyHeader>
-                        <TableBody>
-                            <CidsRow
-                                {...props}
-                                onRowToggle={() => console.log("toggle")}
-                                onEditClick={() => console.log("edit")}
-                                onMoveClick={() => console.log("move")}
-                                onDeleteClick={() => console.log("delete")}
-                            />
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </SnackbarProvider>
-        );
-
-        const tableRow = page.find("tr").at(0);
-        expect(tableRow.hasClass("MuiTableRow-root"))
-        expect(tableRow.hasClass("MuiTableRow-hover"))
-
-        const actionsCell = tableRow.find("td").at(3)
-        expect(actionsCell.hasClass("MuiTableCell-root"))
-
-
-        const editButton = actionsCell.find("svg").at(0)
-        expect(editButton.prop("color")).toBe("black")
-        const moveButton = actionsCell.find("svg").at(1)
-        expect(moveButton.prop("color")).toBe("black")
-        const deleteButton = actionsCell.find("svg").at(2)
-        expect(deleteButton.prop("color")).toBe("black")
-    });
-
-    it("Should change actions color on hover", () => {
-        const page = shallow(
-            <CidsRow
+  it("Should render a CID row", () => {
+    const page = mount(
+      <SnackbarProvider>
+        <TableContainer>
+          <Table size={"small"} stickyHeader>
+            <TableBody>
+              <CidsRow
                 {...props}
                 onRowToggle={() => console.log("toggle")}
                 onEditClick={() => console.log("edit")}
                 onMoveClick={() => console.log("move")}
                 onDeleteClick={() => console.log("delete")}
-            />
-        );
+              />
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </SnackbarProvider>
+    );
 
-        const row = page.find(<TableRow />);
-        expect(row.exists).toBeTruthy()
+    const tableRow = page.find("tr").at(0);
+    expect(tableRow.hasClass("MuiTableRow-root"));
+    expect(tableRow.hasClass("MuiTableRow-hover"));
 
-        let editIcon = page.find('FontAwesomeIcon').at(0)
-        let moveIcon = page.find('FontAwesomeIcon').at(1)
-        let deleteIcon = page.find('FontAwesomeIcon').at(2)
+    const actionsCell = tableRow.find("td").at(3);
+    expect(actionsCell.hasClass("MuiTableCell-root"));
 
-        expect(editIcon.prop('color')).toBe('black')
-        expect(moveIcon.prop('color')).toBe('black')
-        expect(deleteIcon.prop('color')).toBe('black')
+    const editButton = actionsCell.find("svg").at(0);
+    expect(editButton.prop("color")).toBe("black");
+    const moveButton = actionsCell.find("svg").at(1);
+    expect(moveButton.prop("color")).toBe("black");
+    const deleteButton = actionsCell.find("svg").at(2);
+    expect(deleteButton.prop("color")).toBe("black");
+  });
 
-        page.simulate("mouseenter")
+  it("Should change actions color on hover", () => {
+    const page = shallow(
+      <CidsRow
+        {...props}
+        onRowToggle={() => console.log("toggle")}
+        onEditClick={() => console.log("edit")}
+        onMoveClick={() => console.log("move")}
+        onDeleteClick={() => console.log("delete")}
+      />
+    );
 
-        editIcon = page.find('FontAwesomeIcon').at(0)
-        moveIcon = page.find('FontAwesomeIcon').at(1)
-        deleteIcon = page.find('FontAwesomeIcon').at(2)
+    console.log(page.debug());
 
-        expect(editIcon.prop('color')).toBe('blue')
-        expect(moveIcon.prop('color')).toBe('orange')
-        expect(deleteIcon.prop('color')).toBe('red')
+    const row = page.find(<TableRow />);
+    expect(row.exists).toBeTruthy();
 
-        page.simulate("mouseleave")
+    let editIcon = page.find("FontAwesomeIcon").at(0);
+    let moveIcon = page.find("FontAwesomeIcon").at(1);
+    let deleteIcon = page.find("FontAwesomeIcon").at(2);
 
-        editIcon = page.find('FontAwesomeIcon').at(0)
-        moveIcon = page.find('FontAwesomeIcon').at(1)
-        deleteIcon = page.find('FontAwesomeIcon').at(2)
+    expect(editIcon.prop("color")).toBe("black");
+    expect(moveIcon.prop("color")).toBe("black");
+    expect(deleteIcon.prop("color")).toBe("black");
 
-        expect(editIcon.prop('color')).toBe('black')
-        expect(moveIcon.prop('color')).toBe('black')
-        expect(deleteIcon.prop('color')).toBe('black')
-    })
+    page.simulate("mouseenter");
 
-    it("Should trigger callbacks", () => {
-        const toggleMock = jest.fn()
-        const editMock = jest.fn()
-        const moveMock = jest.fn()
-        const deleteMock = jest.fn()
+    editIcon = page.find("FontAwesomeIcon").at(0);
+    moveIcon = page.find("FontAwesomeIcon").at(1);
+    deleteIcon = page.find("FontAwesomeIcon").at(2);
 
-        const page = shallow(
-            <CidsRow
-                {...props}
-                onRowToggle={toggleMock}
-                onEditClick={editMock}
-                onMoveClick={moveMock}
-                onDeleteClick={deleteMock}
-            />
-        );
+    expect(editIcon.prop("color")).toBe("blue");
+    expect(moveIcon.prop("color")).toBe("orange");
+    expect(deleteIcon.prop("color")).toBe("red");
 
-        const row = page.find('WithStyles(ForwardRef(TableRow))').at(0)
-        expect(row.exists).toBeTruthy()
+    page.simulate("mouseleave");
 
-        let editButton = page.find('WithStyles(ForwardRef(IconButton))').at(0)
-        editButton.simulate("click", {
-            stopPropagation: () => {}
-        })
-        expect(editMock).toHaveBeenCalledTimes(1)
+    editIcon = page.find("FontAwesomeIcon").at(0);
+    moveIcon = page.find("FontAwesomeIcon").at(1);
+    deleteIcon = page.find("FontAwesomeIcon").at(2);
 
-        let moveButton = page.find('WithStyles(ForwardRef(IconButton))').at(1)
-        moveButton.simulate("click", {
-            stopPropagation: () => {}
-        })
-        expect(moveMock).toHaveBeenCalledTimes(1)
+    expect(editIcon.prop("color")).toBe("black");
+    expect(moveIcon.prop("color")).toBe("black");
+    expect(deleteIcon.prop("color")).toBe("black");
+  });
 
-        let deleteButton = page.find('WithStyles(ForwardRef(IconButton))').at(2)
-        deleteButton.simulate("click", {
-            stopPropagation: () => {}
-        })
-        expect(deleteMock).toHaveBeenCalledTimes(1)
+  it("Should trigger callbacks", () => {
+    const toggleMock = jest.fn();
+    const editMock = jest.fn();
+    const moveMock = jest.fn();
+    const deleteMock = jest.fn();
 
-        row.simulate("click")
-        expect(toggleMock).toHaveBeenCalledTimes(1)
-    })
-})
+    const page = shallow(
+      <CidsRow
+        {...props}
+        onRowToggle={toggleMock}
+        onEditClick={editMock}
+        onMoveClick={moveMock}
+        onDeleteClick={deleteMock}
+      />
+    );
+
+    const row = page.find("WithStyles(ForwardRef(TableRow))").at(0);
+    expect(row.exists).toBeTruthy();
+
+    let editButton = page.find("WithStyles(ForwardRef(IconButton))").at(0);
+    editButton.simulate("click", {
+      stopPropagation: () => {},
+    });
+    expect(editMock).toHaveBeenCalledTimes(1);
+
+    let moveButton = page.find("WithStyles(ForwardRef(IconButton))").at(1);
+    moveButton.simulate("click", {
+      stopPropagation: () => {},
+    });
+    expect(moveMock).toHaveBeenCalledTimes(1);
+
+    let deleteButton = page.find("WithStyles(ForwardRef(IconButton))").at(2);
+    deleteButton.simulate("click", {
+      stopPropagation: () => {},
+    });
+    expect(deleteMock).toHaveBeenCalledTimes(1);
+
+    row.simulate("click");
+    expect(toggleMock).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/pages/Filters/FilterPage.test.tsx
@@ -0,0 +1,99 @@
+import App from "../App";
+import React from "react";
+import { configure, mount, shallow } from "enzyme";
+import { CidItem, Visibility } from "./Interfaces";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { SnackbarProvider } from "notistack";
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableRow,
+  IconButton,
+} from "@material-ui/core";
+import { useSnackbar, VariantType, WithSnackbarProps } from "notistack";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FilterPage from "./FilterPage";
+import ApiService from "../../services/ApiService";
+jest.mock("../../services/ApiService");
+
+configure({ adapter: new Adapter() });
+
+const mockEnqueue = jest.fn();
+jest.mock("notistack", () => ({
+  ...jest.requireActual("notistack"),
+  useSnackbar: () => {
+    return {
+      enqueueSnackbar: mockEnqueue,
+    };
+  },
+}));
+
+describe("Cid row tests", () => {
+  it("Should render the filter page with the active/inactive toggle", () => {
+    (ApiService.getFilter as jest.Mock).mockResolvedValueOnce({
+      created: "2021-09-29T11:40:19.541Z",
+      updated: null,
+      id: 2,
+      name: "asdqwe",
+      description: "adsasdadsdaszxczxc",
+      override: false,
+      enabled: true,
+      visibility: 1,
+      shareId: "6090-756f-22b8-c953",
+      cids: [],
+      provider: {
+        created: "2021-09-29T11:38:17.913Z",
+        updated: "2021-09-29T11:38:25.326Z",
+        id: 201,
+        walletAddressHashed:
+          "30cf5b73fcf97e92c7f5ff85d417981bcf9f487f66d1c0b6289f02b1f6959997",
+        country: null,
+        businessName: null,
+        website: null,
+        email: null,
+        contactPerson: null,
+        address: null,
+        nonce: "4317964d-8083-41be-a6b4-8c15773f3a70",
+      },
+      provider_Filters: [
+        {
+          created: "2021-09-29T11:40:19.573Z",
+          updated: "2021-09-29T15:28:13.705Z",
+          id: 2,
+          active: true,
+          notes: null,
+          provider: {
+            created: "2021-09-29T11:38:17.913Z",
+            updated: "2021-09-29T11:38:25.326Z",
+            id: 201,
+            walletAddressHashed:
+              "30cf5b73fcf97e92c7f5ff85d417981bcf9f487f66d1c0b6289f02b1f6959997",
+            country: null,
+            businessName: null,
+            website: null,
+            email: null,
+            contactPerson: null,
+            address: null,
+            nonce: "4317964d-8083-41be-a6b4-8c15773f3a70",
+          },
+        },
+      ],
+    });
+
+    (ApiService.getProviderConfig as jest.Mock).mockResolvedValueOnce({
+      id: 5,
+      bitscreen: true,
+      import: false,
+      share: false,
+    });
+
+    const page = shallow(
+      <FilterPage
+        match={{ params: { shareId: 1 }, isExact: true, path: "", url: "" }}
+      />
+    );
+    console.log(page.debug());
+    expect(page.text().includes("INACTIVE")).toBe(true);
+  });
+});
