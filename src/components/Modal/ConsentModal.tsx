a4a9ce8f9861fc2c9965f45bbd50b387b04d4bb0
Stefan Manciu <manciustefan@gmail.com>
2021-11-10 14:35:16 +0200

    BTSC-270 Added consent modal

---
+++ src/components/Modal/ConsentModal.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Modal, Button, ListGroup, Form } from "react-bootstrap";
+import { Link } from "@material-ui/core";
+
+export interface ConsentModalProps {
+  show: boolean;
+  callback: (consent: boolean) => void;
+  closeCallback: () => void;
+}
+
+export default function ConsentModal(props: ConsentModalProps): JSX.Element {
+  const [consentValue, setConsentValue] = useState<boolean>(false);
+
+  const handleCheckboxChange = (event) => {
+    setConsentValue(event.target.checked);
+  };
+
+  return (
+    <Modal
+      show={props.show}
+      onHide={() => {
+        props.closeCallback();
+      }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Welcome to BitScreen</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          By using BitScreen, you agree to our{" "}
+          <Link
+            href="https://github.com/Murmuration-Labs/bitscreen/blob/master/terms_of_service.md"
+            target="_blank"
+          >
+            Terms of Service
+          </Link>{" "}
+          &{" "}
+          <Link
+            href="https://github.com/Murmuration-Labs/bitscreen/blob/master/privacy_policy.md"
+            target="_blank"
+          >
+            Privacy Policy
+          </Link>
+          . If you do not agree, you cannot use BitScreen.
+        </p>
+        <Form.Group className="mb-3" controlId="consentCheckbox">
+          <Form.Check
+            onChange={handleCheckboxChange}
+            type="checkbox"
+            label="I have read and agree."
+          />
+        </Form.Group>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            props.closeCallback();
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            props.callback(consentValue);
+            props.closeCallback();
+          }}
+        >
+          Continue
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
