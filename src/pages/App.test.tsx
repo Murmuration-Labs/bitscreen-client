75349af860ad0b0a043f7e3b612733b584fcbf41
Stefan Manciu <manciustefan@gmail.com>
2021-12-07 10:51:33 +0200

    Fixed App tests

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { configure, mount } from "enzyme";
 import App from "./App";
+import { Router } from "react-router-dom";
+import history from "../history";
 
 configure({ adapter: new Adapter() });
 
 describe("App Routing Test", () => {
   it("Should render settings page on homepage", () => {
-    const page = mount(<App />);
+    const page = mount(<Router history={history}><App /></Router>);
 
     const settingsMenuEntry = page.find("a").at(0);
 
@@ -15,7 +17,7 @@ describe("App Routing Test", () => {
   });
 
   it("Should render filters page on /filters", () => {
-    const page = mount(<App />);
+    const page = mount(<Router history={history}><App /></Router>);
 
     const filtersMenuEntry = page.find("a").at(1);
 
@@ -24,7 +26,7 @@ describe("App Routing Test", () => {
   });
 
   it("Should render settings page on /settings after page switch", () => {
-    const page = mount(<App />);
+    const page = mount(<Router history={history}><App /></Router>);
 
     const filtersMenuEntry = page.find("a").at(1);
 
