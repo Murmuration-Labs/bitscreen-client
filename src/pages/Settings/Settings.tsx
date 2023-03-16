ac5c76d6c66f408c198c607469ce8e973f2ae70a
Alexandru-Marian Constantin <alexm.constantin@gmail.com>
2023-03-16 14:34:16 +0200

    fix: changed spacing between logout and link buttons

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -661,7 +661,7 @@ export default function Settings(props) {
               className="wallet-status-actions d-flex justify-content-between align-items-center"
             >
               <div className="d-flex">
-                <div className="logout-button mr-3">
+                <div className="logout-button mr-12px">
                   <Button
                     onClick={logout}
                     variant="primary"
